0f39711c8c2f18b138251c60474d0d1167b523a1
Adam Goode <agoode@andrew.cmu.edu>
2008-11-14 17:04:38 -0500

    Add license

--- client/src/edu/cmu/cs/diamond/example/StringFind.java
+++ client/src/edu/cmu/cs/diamond/example/StringFind.java
@@ -1,3 +1,43 @@
+/*
+ * Diamond example client
+ *
+ * This code is licensed under a permissive license and is meant to be
+ * copied and incorporated into other projects.
+ *
+ * Copyright (c) 2008, Carnegie Mellon University
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above
+ *    copyright notice, this list of conditions and the following
+ *    disclaimer in the documentation and/or other materials provided
+ *    with the distribution.
+ *
+ *  * Neither the name of the Carnegie Mellon University nor the names
+ *    of its contributors may be used to endorse or promote products
+ *    derived from this software without specific prior written
+ *    permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package edu.cmu.cs.diamond.example;
 
 import java.io.File;
@@ -45,7 +85,7 @@ public class StringFind {
             s.setScope(ScopeSource.getPredefinedScopeList().get(0));
             s.setSearchlet(searchlet);
             s.start();
-            
+
             // results
             Result r;
             while ((r = s.getNextResult()) != null) {
